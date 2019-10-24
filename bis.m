f38753b3281de9c4149e60a6eadfd207e3d01c9e
SaulSaavedra <55523447+SaulSaavedra@users.noreply.github.com>
2019-10-24 17:30:22 -0500

    Add files via upload

---
+++ bis.m
@@ -0,0 +1,39 @@
+xl = 0;
+xu = 2;
+i = 1;
+e(i) = 10;
+
+while(e(i)> 0.01)
+xr = (xl+xu)/2;
+
+g_xl = xl^2-2;
+g_xr = xr^2-2;
+g_xu = xu^2-2;
+
+if(g_xl*g_xr < 0)
+    e(i+1) = abs(xr - xl);
+end
+    xu = xr;
+
+if(g_xu*g_xr <0)
+    xl = xr;
+    e(i+1)= abs( xu -xr);
+end
+i = i + 1;
+if(g_xu*g_xr == 0)
+    xr = xr;
+end
+end
+
+tamanio = size (e);
+ite = 1:1:tamanio(2);
+
+figure('DefaultAxesFontSize',14)
+set(gcf,'color', 'white');
+plot(ite,e,'color',[1 0 0],'linewidth',2);
+grid on
+hold on
+plot(ite,x,'color',[0 0 1],'linewidth',2);
+xlabel('Iterations')
+ylabel('error')
+legend('error')
---
+++ pf.m
@@ -0,0 +1,30 @@
+init_value = 1
+i=1;
+e(i)= 10;
+while(e(i)>0.001)
+if(i == 1)
+    x(i) = init_value;
+end
+g(i)= real(sqrt((5)/(x(i)-10)));
+i = i + 1
+x(i) = g(i-1);
+e(i) = abs(x(i-1)-x(i));
+if (e(i) > 50);
+    break;
+end
+end
+
+
+
+tamanio = size (e);
+ite = 1:1:tamanio(2);
+
+figure('DefaultAxesFontSize',14)
+set(gcf,'color', 'white');
+plot(ite,e,'color',[1 0 1],'linewidth',2);
+grid on
+hold on
+plot(ite,x,'color',[0 0 1],'linewidth',2);
+xlabel('Iterations')
+ylabel('error')
+legend('error')
